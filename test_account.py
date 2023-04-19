b73416fe6a880bbb66f9a4eac139f8306d79fbb6
asteele314 <131196316+asteele314@users.noreply.github.com>
2023-04-19 14:22:40 -0600

    Update test_account.py

--- test_account.py
+++ test_account.py
@@ -2,21 +2,42 @@ import pytest
 from account import *
 
 
-account1 = Account('name')
-
-
-def test_init():
-    assert account1.__account_name == 'name'
-    assert account1.__account_balance == 0
-
-
-def test_deposit():
-    assert account1.deposit(-10) is False
-    assert account1.deposit(0) is False
-    assert account1.deposit(100) is True
-
-
-def test_withdraw():
-    assert account1.withdraw(-10) is False
-    assert account1.withdraw(0) is False
-    assert account1.withdraw(10) is True
+class Test:
+    def setup_method(self):
+        self.account1 = Account('Name1')
+    
+    
+    def teardown_method(self):
+        del self.account1
+    
+    
+    def test_init(self):
+        assert self.account1.get_name() == 'Name1'
+        assert self.account1.get_balance() == 0
+    
+    
+    def test_deposit(self):
+        assert self.account1.deposit(-10) is False
+        assert self.account1.get_balance() == 0
+        assert self.account1.deposit(0) is False
+        assert self.account1.get_balance() == 0
+        assert self.account1.deposit(10) is True
+        assert self.account1.get_balance() == 10
+        assert self.account1.deposit(107.5) is True
+        assert self.account1.get_balance() == 117.5
+        self.account1.withdraw(117.5)
+    
+    
+    def test_withdraw(self):
+        self.account1.deposit(100)
+        assert self.account1.withdraw(-25) is False
+        assert self.account1.get_balance() == 100
+        assert self.account1.withdraw(0) is False
+        assert self.account1.get_balance() == 100
+        assert self.account1.withdraw(250) is False
+        assert self.account1.get_balance() == 100
+        assert self.account1.withdraw(20) is True
+        assert self.account1.get_balance() == 80
+        assert self.account1.withdraw(37.5) is True
+        assert self.account1.get_balance() == 42.5
+        
